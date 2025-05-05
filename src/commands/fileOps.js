827df8fb639e938f60b5c21cffd45c25a8d5c366
Fluret <akuntsievich@inbox.ru>
2025-05-05 21:18:35 +0300

    add basics operation

--- src/commands/fileOps.js
+++ src/commands/fileOps.js
@@ -1,5 +1,8 @@
 import fs from 'fs';
+import { createReadStream, createWriteStream } from 'fs';
 import path from 'path';
+
+
 
 export async function cat(filePath, currentDir) {
     const fullPath = path.resolve(currentDir, filePath);
@@ -33,6 +36,53 @@ export async function mkdir(dirName, currentDir) {
     try {
         await fs.promises.mkdir(dirPath);
         console.log(`Directory "${dirName}" created successfully.`);
+    } catch (err) {
+        console.error('Operation failed');
+    }
+}
+export async function rn(oldPath, newPath, currentDir) {
+    const oldFullPath = path.resolve(currentDir, oldPath);
+    const newFullPath = path.resolve(currentDir, newPath);
+
+    try {
+        await fs.promises.rename(oldFullPath, newFullPath);
+        console.log(`"${oldPath}" renamed to "${newPath}" successfully.`);
+    } catch (err) {
+        console.error('Operation failed');
+    }
+}
+export async function cp(sourcePath, destinationPath, currentDir) {
+    const sourceFullPath = path.resolve(currentDir, sourcePath);
+    const destinationFullPath = path.resolve(currentDir, destinationPath);
+
+    try {
+        const readStream = createReadStream(sourceFullPath);
+        const writeStream = createWriteStream(destinationFullPath);
+
+        readStream.pipe(writeStream);
+
+        readStream.on('error', () => console.error('Operation failed'));
+        writeStream.on('error', () => console.error('Operation failed'));
+        writeStream.on('finish', () => console.log('File copied successfully.'));
+    } catch (err) {
+        console.error('Operation failed');
+    }
+}
+export async function mv(sourcePath, destinationPath, currentDir) {
+    try {
+        await cp(sourcePath, destinationPath, currentDir); // Копируем файл
+        await rm(sourcePath, currentDir); // Удаляем исходный файл
+        console.log('File moved successfully.');
+    } catch (err) {
+        console.error('Operation failed');
+    }
+}
+export async function rm(filePath, currentDir) {
+    const fullPath = path.resolve(currentDir, filePath);
+
+    try {
+        await fs.promises.unlink(fullPath);
+        console.log(`File "${filePath}" deleted successfully.`);
     } catch (err) {
         console.error('Operation failed');
     }
